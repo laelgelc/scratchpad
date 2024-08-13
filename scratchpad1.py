cf12b0ef8d0f1fc0a5b11182e247f456970d144d
eyamrog <eyamrog@gmail.com>
2024-08-13 19:23:07 -0300

    20240813-1923

--- scratchpad1.py
+++ scratchpad1.py
@@ -1,17 +1,28 @@
-#hint
-#create dataframe
-data_CI = .............
-#Set up the Waffle chart figure
+#The correct answer is:
 
-fig = plt.figure(FigureClass = ............,
-                 rows = ........, columns =....., #pass the number of rows and columns for the waffle 
-                 values = ........., #pass the data to be used for display
-                 cmap_name = 'tab20', #color scheme
-                 legend = {'labels':[.......],
-                            'loc': ........., 'bbox_to_anchor':(....),'ncol': 2}
-                 #notice the use of list comprehension for creating labels 
-                 #from index and total of the dataset
-                )
+# create df_countries dataframe
+df_countries = df_can.loc[['Denmark', 'Norway', 'Sweden'], years].transpose()
 
-#Display the waffle chart
-plt.show()
+# create df_total by summing across three countries for each year
+df_total = pd.DataFrame(df_countries.sum(axis=1))
+
+# reset index in place
+df_total.reset_index(inplace=True)
+
+# rename columns
+df_total.columns = ['year', 'total']
+
+# change column year from string to int to create scatter plot
+df_total['year'] = df_total['year'].astype(int)
+
+# define figure size
+plt.figure(figsize=(15, 10))
+
+# define background style and font size
+sns.set(font_scale=1.5)
+sns.set_style('whitegrid')
+
+# generate plot and add title and axes labels
+ax = sns.regplot(x='year', y='total', data=df_total, color='green', marker='+', scatter_kws={'s': 200})
+ax.set(xlabel='Year', ylabel='Total Immigration')
+ax.set_title('Total Immigrationn from Denmark, Sweden, and Norway to Canada from 1980 - 2013')
