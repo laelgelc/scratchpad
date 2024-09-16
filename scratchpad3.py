491938facb64d5a20fb0845fbe0ae1b339f419b1
eyamrog <eyamrog@gmail.com>
2024-09-16 12:04:09 -0300

    20240916-1204

--- scratchpad3.py
+++ scratchpad3.py
@@ -5,22 +5,4 @@ IBM_Course
 
 10_Data_Science_Capstone_Lab1_Collecting_the_Data
 
-
-launch_dict= dict.fromkeys(column_names)
-
-# Remove an irrelevant column
-del launch_dict['Date and time ( )']
-
-# Let's initial the launch_dict with each value to be an empty list
-launch_dict['Flight No.'] = []
-launch_dict['Launch site'] = []
-launch_dict['Payload'] = []
-launch_dict['Payload mass'] = []
-launch_dict['Orbit'] = []
-launch_dict['Customer'] = []
-launch_dict['Launch outcome'] = []
-# Added some new columns
-launch_dict['Version Booster']=[]
-launch_dict['Booster landing']=[]
-launch_dict['Date']=[]
-launch_dict['Time']=[]
+10_Data_Science_Capstone__Lab2_Data_Wrangling
