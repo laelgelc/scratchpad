2dd3ccc98790245b708af1eaa6415987afb24272
eyamrog <eyamrog@gmail.com>
2024-09-19 15:58:23 -0300

    20240919-1558

--- scratchpad1.py
+++ scratchpad1.py
@@ -2,6 +2,6 @@ re.sub(r'\s+', ' ', tokenised_line)
 re.sub(r'\s{2,}', ' ', text)
 
 
-Dear ChatGPT, would it be possible for you to improve the writing of the following passage of a research article considering the generally accepted standards of English for Academic Purposes? Please keep each improved passage within a single paragraph - do not split it into multiple paragraphs. OK?
+prompt = 'Dear ChatGPT, would it be possible for you to improve the writing of the following passage of a research article considering the generally accepted standards of English for Academic Purposes? Please keep each improved passage within a single paragraph - do not split it into multiple paragraphs. OK?\n'
     
 Dear ChatGPT, please write a piece of academic text based on the following notes considering the generally accepted standards of English for Academic Purposes. It is very important that you are as objective, scientific and non-metaphorical as you can be.
